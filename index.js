12f284d190cacdca3d079d8f347610a728bdbe9f
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-11-21 15:44:37 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: 0BSD
 
 // 定义DNS over HTTPS (DoH)的端点
-const doh = 'https://doh.pub/dns-query'
-const dohjson = 'https://doh.pub/resolve'
+const doh = 'https://doh.apad.pro/dns-query'
+const dohjson = 'https://doh.apad.pro/dns-query'
 
 // 定义内容类型
 const contype = 'application/dns-message'
